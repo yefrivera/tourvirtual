ebf708305f8c5eae70db76432fd221820579499c
yefrivera <yvera@unicauca.edu.co>
2024-05-31 13:23:00 -0500

    pop

--- popayan.js
+++ popayan.js
@@ -40,7 +40,7 @@ function init() {
     sphereGeometry.scale(-1, 1, 1); 
     const textureLoader = new THREE.TextureLoader();
     textureLoader.setPath('./textures/');
-    const texture = textureLoader.load('popayán.jpg', function (texture) {
+    const texture = textureLoader.load('360p.jpg', function (texture) {
         texture.colorSpace= THREE.SRGBColorSpace; 
     });
 
